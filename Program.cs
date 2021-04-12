20d09a8cc5c8e8ac0f338481dd018bf9eaf83fc4
Orsós Lívia <orsoslivia@hotmail.com>
2021-04-12 13:24:40 +0200

    generate cs code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
+using Scriban;
 
 namespace cs
 {
     class Program
     {
+        class ClassProperty
+        {
+            public string Name { get; set; }
+            public bool IsServerOnly { get; set; }
+            public string Type { get; set; }
+        }
+        class Model
+        {
+            public string Name { get; set; }
+            public ClassProperty[] Properties { get; set; }
+        }
+        static ClassProperty[] userProperties = { 
+            new ClassProperty { Name = "Id", IsServerOnly = false, Type = "int" },
+            new ClassProperty { Name = "UserName", IsServerOnly = false, Type = "string" },
+            new ClassProperty { Name = "Email", IsServerOnly = false, Type = "string" },
+            new ClassProperty { Name = "PasswordHash", IsServerOnly = true, Type = "string" },
+        };
+        static Model user = new Model { Name = "User", Properties = userProperties };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileName = "template.sbn";
+            var content = File.ReadAllText(fileName);
+            var template = Template.Parse(content, fileName);
+            var result = template.Render(new { 
+                NameSpace = "CsExample",
+                Class = user
+            });
+            
+            var path = @".\user.cs";
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.Write(result);
+            }
         }
     }
 }
